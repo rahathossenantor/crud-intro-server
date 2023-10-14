5fc3a6ecdb3adf57ef5e8794fddfa7c4f852f479
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-14 14:24:26 +0600

    Learned how to create, read and delete data from DB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 
 const server = express();
 const port = process.env.PORT || 5000;
@@ -23,6 +23,31 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("usersDB");
+        const userCollection = database.collection("users");
+
+        server.get("/users", async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        server.post("/users", async (req, res) => {
+            const user = req.body;
+            // console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        server.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
